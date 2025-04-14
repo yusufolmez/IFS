ff44e18f4e8e24da7d5aef7cdadfb40821860c43
Yusuf Ölmez <yusuf.olmez670@gmail.com>
2025-04-14 12:13:59 +0300

    Implement JWT authentication middleware; add custom permission handling and token blacklisting; enhance user management with custom roles and permissions; update GraphQL schema and user creation logic; modify settings for JWT integration.

--- core/userManage/models.py
+++ core/userManage/models.py
@@ -2,21 +2,48 @@ from django.db import models
 from django.contrib.auth.hashers import make_password, check_password
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
+class CustomPermission(models.Model):
+    name = models.CharField(max_length=50)
+    codename = models.CharField(max_length=150)
+    description = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+class CustomRole(models.Model):
+    name = models.CharField(max_length=50)
+    permissions = models.ManyToManyField(CustomPermission, blank=True)
+    description = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def get_permission(self):
+        return [perm.codename for perm in self.permissions.all()]
+
+    def __str__(self):
+        return self.name
+
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, email, password=None, **extra_fields):
         if not email:
-            raise ValueError('The Email field must be set')
+            raise ValueError('Email adresi gereklidir')
+        if not username:
+            raise ValueError('Kullanıcı adı gereklidir')
+        
         email = self.normalize_email(email)
         user = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
-
+    
     def create_superuser(self, username, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
         
         return self.create_user(username, email, password, **extra_fields)
+    
+    def get_by_natural_key(self, username):
+        return self.get(username=username)  
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -26,15 +53,10 @@ class BaseModel(models.Model):
         abstract = True
 
 class CustomUser(AbstractBaseUser, BaseModel):
-    class RoleChoices(models.TextChoices):
-        Admin = 'admin', 'Admin'
-        Student = 'student', 'Student'
-        Company = 'company', 'Company'
-
     username = models.CharField(max_length=150, unique=True) # kaldırılabilir.
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128)
-    role = models.CharField(max_length=10, choices=RoleChoices.choices)
+    role = models.ForeignKey(CustomRole, on_delete=models.CASCADE, null=True, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
@@ -50,7 +72,12 @@ class CustomUser(AbstractBaseUser, BaseModel):
         verbose_name_plural = 'users'
 
     def has_perm(self, perm, obj=None):
-        return self.is_superuser
+        if self.is_superuser:
+                    return True
+    
+        if self.role:
+            return perm in self.role.get_permission()
+        return False
 
     def has_module_perms(self, app_label):
         return self.is_superuser
--- core/userManage/schema.py
+++ core/userManage/schema.py
@@ -1,8 +1,51 @@
-import base64
+from datetime import datetime, timedelta
+from django.conf import settings
+from django.contrib.auth import authenticate
 import graphene
+import jwt
+from functools import wraps
+import base64
 from graphene_django.types import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
-from .models import CustomUser, Student, Company
+from .models import CustomRole, CustomUser, Student, Company
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def custom_permission_required(required_permiision):
+    def decoreator(func):
+        @wraps(func)
+        def wrapper(root,info,*args,**kwargs):
+            
+            user = info.context.user
+            if not user.is_authenticated:
+                raise Exception("Lütfen giriş yapınız.")
+            if not user.has_permission(required_permiision):
+                raise Exception('Yetkiniz yok')
+            return func(root, info,*args,**kwargs)
+        return wrapper
+    return decoreator
+
+def generate_access_token(user):
+    payload = {
+        'user_id':user.id,
+        'user_role':user.role.name,
+        'exp': datetime.utcnow() + timedelta(minutes=15),
+        'iat': datetime.utcnow(),
+        'token_type':'access',
+    }
+    return jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+
+def generate_refresh_token(user):
+    payload = {
+        'user_id':user.id,
+        'user_role':user.role.name,
+        'exp': datetime.utcnow() + timedelta(days=7),
+        'iat': datetime.utcnow(),
+        'token_type':'refresh',
+    }
+    return jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class CustomUserNode(DjangoObjectType):
     class Meta:
@@ -28,13 +71,41 @@ class StudentNode(DjangoObjectType):
         }
         interfaces = (graphene.relay.Node,)
 
+class TokenType(graphene.ObjectType):
+    access_token = graphene.String()
+    refresh_token = graphene.String()
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'role', 'is_active', 'is_staff', 'is_superuser')
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+class AuthMutation(graphene.Mutation):
+    tokens = graphene.Field(TokenType)
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+    
+    def mutate(self,info,username,password):
+        user = authenticate(username=username,password=password)
+
+        if user is None:
+            raise Exception("Gecersiz giris bilgileri!")
+        access_token = generate_access_token(user)
+        refresh_token = generate_refresh_token(user)
+
+        return AuthMutation(tokens=TokenType(access_token=access_token,refresh_token=refresh_token))
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 class CreateUserMutation(graphene.Mutation):
     class Arguments:
         username = graphene.String(required=True)
         email = graphene.String(required=True)
         password = graphene.String(required=True)
-        role = graphene.String(required=True)
+        role_id = graphene.ID(required=True)
+
+        user_type = graphene.String(required=True)
 
         first_name = graphene.String(required=False)
         last_name = graphene.String(required=False)
@@ -52,41 +123,64 @@ class CreateUserMutation(graphene.Mutation):
         phone_number = graphene.String(required=False)
         address = graphene.String(required=False)
 
-    # user = graphene.Field(CustomUserNode)
+    user = graphene.Field(lambda: UserType)
+    success = graphene.Boolean()
     message = graphene.String()
 
-    def mutate(self, info, username, email, password, role, first_name=None, last_name=None, student_number=None, department=None, faculty=None, date_of_birth=None, profile_picture=None, company_name=None, contact_person=None, website=None, tax_number=None, phone_number=None, address=None):
+    @classmethod
+    def mutate(cls, root, info, username, email, password, role_id, user_type, first_name=None, last_name=None, student_number=None, department=None, faculty=None, date_of_birth=None, profile_picture=None, company_name=None, contact_person=None, website=None, tax_number=None, phone_number=None, address=None, **kwargs):
         try:
+            try:
+                role = CustomRole.objects.get(id=role_id)
+            except CustomRole.DoesNotExist:
+                return CreateUserMutation(success=False, message="Role bulunamadi")
+            
             user = CustomUser.objects.create_user(
-            username=username,
-            email=email,
-            password=password,
-            role=role
+                username=username,
+                email=email,
+                password=password,
+                role=role
             )
-            if role == "admin":
-                pass
-            if role == "student":
-                student = Student.objects.create(user=user, 
-                                                first_name=first_name,
-                                                last_name=last_name,
-                                                student_number=student_number,
-                                                department=department,
-                                                faculty=faculty,
-                                                date_of_birth=date_of_birth,
-                                                profile_picture=profile_picture,
-                                                phone_number=phone_number,
-                                                address=address)
-                student.save()
-            if role == "company":
-                company = Company.objects.create(user=user, 
-                                                company_name=company_name,
-                                                contact_person=contact_person,
-                                                website=website,
-                                                tax_number=tax_number,
-                                                phone_number=phone_number,
-                                                address=address)
-                company.save()
             user.save()
+
+            if user_type.lower() == 'admin':
+                user.is_superuser = True
+                user.is_staff = True
+                user.save()
+
+            elif user_type.lower() == 'student':
+                required_fields = ['first_name', 'last_name', 'student_number', 'department', 'faculty']
+                for field in required_fields:
+                    if field not in kwargs or not kwargs[field]:
+                        user.delete()
+                        return CreateUserMutation(success=False, message=f"Alan {field} ogrenci tipte zorunludur.")
+                Student.objects.create(
+                                    user=user,
+                                    first_name=kwargs.get('first_name'),
+                                    last_name=kwargs.get('last_name'),
+                                    student_number=kwargs.get('student_number'),
+                                    department=kwargs.get('department'),
+                                    faculty=kwargs.get('faculty'),
+                                    phone_number=kwargs.get('phone_number'),
+                                    address=kwargs.get('address'),
+                                    date_of_birth=kwargs.get('date_of_birth')
+                                )
+            elif user_type.lower() == 'company':
+                required_fields = ['company_name', 'contact_person']
+                for field in required_fields:
+                    if field not in kwargs or not kwargs[field]:
+                        user.delete()
+                        return CreateUserMutation(success=False, message=f"Alan {field} şirket tipte zorunludur.")
+                Company.objects.create(
+                                    user=user,
+                                    company_name=kwargs.get('company_name'),
+                                    contact_person=kwargs.get('contact_person'),
+                                    phone_number=kwargs.get('company_phone'),
+                                    address=kwargs.get('company_address'),
+                                    website=kwargs.get('website'),
+                                    tax_number=kwargs.get('tax_number')
+                                )
+            
             return CreateUserMutation(message = "User created successfully")
         except Exception as e:
             raise Exception(f"Error creating user: {str(e)}")
@@ -109,5 +203,11 @@ class UserManageQuery(graphene.ObjectType):
                 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 class UserManageMutation(graphene.ObjectType):
+    auth = AuthMutation.Field()
+
+
+
+    
     userCreate = CreateUserMutation.Field()
+
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
